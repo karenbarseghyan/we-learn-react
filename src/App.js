9a5439c745fc2c0ea5a42b027dab14fae112f913
user.email <karenbarseghyan90@gmail.com>
2020-10-05 19:28:55 +0400

    State and Subscribe-Button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome'
+// import Greet from './components/Greet'
+// import Welcome from './components/Welcome'
+import Message from './components/Message'
 
 function App() {
   return (
     <div className="App">
-          <Greet name = "Karen" heroname = "Nicki"/>
-          <Greet name = "Ruben" heroname = "Robert"/>
-          <Greet name = "Leo" heroname = "Messi"/>
-          <Welcome name = "Karen" heroname = "Nicki"/>
-          <Welcome name = "Ruben" heroname = "Robert"/>
-          <Welcome name = "Leo" heroname = "Messi"/>
+      <Message />
+          {/* <Greet name = "Karen" heroname = "Nicki"/> */}
+          {/* <Greet name = "Ruben" heroname = "Robert"/> */}
+          {/* <Greet name = "Leo" heroname = "Messi"/> */}
+          {/* <Welcome name = "Karen" heroname = "Nicki"/> */}
+          {/* <Welcome name = "Ruben" heroname = "Robert"/> */}
+          {/* <Welcome name = "Leo" heroname = "Messi"/> */}
     </div>
   );
 }
