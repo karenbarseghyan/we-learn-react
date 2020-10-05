a3157ae222264851f3c896391d3dbbcf83694d24
user.email <karenbarseghyan90@gmail.com>
2020-10-06 00:21:48 +0400

    Event Hadnling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import './App.css';
-import Greet from './components/Greet'
-import Welcome from './components/Welcome'
+// import Greet from './components/Greet'
+// import Welcome from './components/Welcome'
 // import Message from './components/Message'
 // import Message2 from './components/Message2'
 // import Counter from './components/Counter'
+import FunctionClick from './components/FunctionClick'
+import ClassClick from './components/ClassClick'
 
 function App() {
   return (
     <div className="App">
+      <FunctionClick />
+      <ClassClick />
       {/* <Counter /> */}
       {/* <Message /> */}
       {/* <Message2 /> */}
-          <Greet name = "Karen" heroname = "Nicki"/>
+          {/* <Greet name = "Karen" heroname = "Nicki"/> */}
           {/* <Greet name = "Ruben" heroname = "Robert"/> */}
           {/* <Greet name = "Leo" heroname = "Messi"/> */}
-          <Welcome name = "Karen" heroname = "Nicki"/>
+          {/* <Welcome name = "Karen" heroname = "Nicki"/> */}
           {/* <Welcome name = "Ruben" heroname = "Robert"/> */}
           {/* <Welcome name = "Leo" heroname = "Messi"/> */}
     </div>
