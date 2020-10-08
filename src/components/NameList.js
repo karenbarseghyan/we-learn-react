a6c29da7694c006e62d236f3f67c3eadfad59bb9
user.email <karenbarseghyan90@gmail.com>
2020-10-09 00:08:59 +0400

    Inline Regular Modular CSS

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 function NameList() {
-    const names = ['Bruce', 'Diana', 'Clark']
-    const NameList = names.map (name => <h2>{name}</h2>)
+    const names = ['Bruce', 'Diana', 'Clark', 'Bruce']
+    const NameList = names.map ((name,index) => <h2 key ={index}>{index}{name}</h2>)
     return <div> {NameList} </div>
 }
 
