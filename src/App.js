02dd7477e81974ab7619f8f568f15247755be32a
user.email <karenbarseghyan90@gmail.com>
2020-10-10 00:21:18 +0400

    Forms Handling

--- src/App.js
+++ src/App.js
@@ -12,16 +12,18 @@ import './App.css';
 // import UserGreeting from './components/UserGreeting'
 // import NameList from './components/NameList'
 // import Stylesheet from './components/Stylesheet'
-import Inline from './components/Inline'
+// import Inline from './components/Inline'
 import './styles/appStyles.css'
-import styles from './styles/appStyles.module.css'
+// import  styles from './styles/appStyles.module.css'
+import Form from './components/Form'
 
 function App() {
   return (
     <div className="App">
-      <h1 className = 'error'>Error</h1>
-      <h1 className = {styles.success}>Success</h1>
-      <Inline />
+      <Form />
+      {/* <h1 className = 'error'>Error</h1> */}
+      {/* <h1 className = {styles.success}>Success</h1> */}
+      {/* <Inline /> */}
       {/* <Stylesheet primary = {true}/> */}
       {/* <NameList /> */}
       {/* <UserGreeting /> */}
